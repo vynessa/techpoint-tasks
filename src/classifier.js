8211965f90a6fbe95283a0621e70981e34965bf4
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2018-07-30 10:45:12 +0100

    create group metadata

--- src/classifier.js
+++ src/classifier.js
@@ -5,8 +5,15 @@ var input = require('./inputs/input')
  *
  * @param {array} input Array of student objects
  */
+
 const classifier = (input) => {
-  // Your code should go here.
+  //Your code should go here
+  const validateInput = () => {
+    if(!Array.isArray(input)) {
+      throw"Invalid Type. Please enter an Array";
+    }
+    return true;
+  }
 
   const getAge = (birthDay) => {
     var today = new Date();
@@ -14,12 +21,12 @@ const classifier = (input) => {
     var age = today.getFullYear() - birthDate.getFullYear();
     var month = today.getMonth() - birthDate.getMonth();
     if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
-    }
+      age--
+    } 
     return age;
   }
-  
-  const sortAge = (input) => {
+
+  const sortByAge = (input) => {
     Object.keys(input).forEach((student, index) => {
       input[student].age = getAge(input[student].dob)
       delete(input[student].dob);
@@ -28,6 +35,27 @@ const classifier = (input) => {
       return prev.age - next.age;
     });
   }
-}
+
+  const groupMembersMetadata = (members) => {
+    let group = {
+      members: [],
+      oldest: 0,
+      sum: 0,
+      regNos: []
+    }
+    members.forEach(student => { group.members.push(student) });
+    membersAges = group.members.map((student) => student.age);
+    group.oldest = Math.max(...membersAges);
+    group.sum = membersAges.reduce((acc, cur) => (acc + cur));
+    group.regNos = group.members.map((student) => student.regNo).sort(
+      (prev, next) => (prev - next)
+    );
+    group.members.forEach(student => { 
+      delete(student.regNo) });
+    return group
+  }
+};
+
+classifier = JSON.stringify(classifier(input), null, 2);
 
 module.exports = classifier;
