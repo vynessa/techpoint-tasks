0f6281f8ab992ee803556a88e479975dd49077cb
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2018-07-30 22:48:02 +0100

    Fix errors on code breaking test cases

--- src/morse.js
+++ src/morse.js
@@ -63,8 +63,37 @@ Object.freeze(MORSE_CODE);
  *
  * @param {string} morseCode The string to decode.
  */
-function decodeMorse(morseCode) {
+
+const decodeMorse = (morseCode) => {
   // Your code should go here.
-}
+  try {
+    if(typeof morseCode !== 'string') {
+      throw 'Please enter a string';
+    }
+    if(!morseCode.length) return '';
+    morseCode = morseCode.trim();
+    let result = ""
+    const splitMorseString = morseCode.split(" ")
+
+    for (i = 0; i < splitMorseString.length; i++) {
+      let prev = splitMorseString[i+1]
+      let current = splitMorseString[i]
+      if (prev == '' && current == '') {
+        continue;
+      }
+      if(current == ''){
+        result += " "
+        continue;
+      }
+      if(!(MORSE_CODE.hasOwnProperty(splitMorseString[i]))) {
+        return "Morse you decode this? Enter correct code"
+      }
+      result += MORSE_CODE[`${splitMorseString[i]}`]
+    }
+    return result
+  } catch (error) {
+      throw error;
+  }
+};
 
 module.exports = decodeMorse;
